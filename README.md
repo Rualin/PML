108427d463b3f07561badb64348793c4025d4347
Rualin <fateevrual@gmail.com>
2025-09-18 12:01:18 +0700

    Update readme, add requirements.txt

--- README.md
+++ README.md
@@ -2,4 +2,15 @@
 
 It is repository for Practical Machine Learning tasks.
 
-See the tasks in the branches
+## Task 1 (DA-1-21)
+Correlation search between features
+
+### Installation
+Firstly, install dependencies:
+```
+pip install -r requirements.txt
+```
+Then you can run program:
+```
+python3 task.py
+```
--- task.py
+++ task.py
@@ -4,15 +4,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def main():
+def get_dataset() -> pd.DataFrame:
     # Function sklearn.datasets.load_boston is deprecated since scikit-learn 1.2
     # That is alternative from scikit-learn.org
     data_url = "http://lib.stat.cmu.edu/datasets/boston"
+    # 22 is number from scikit-learn.org, first lines of dataset is incorrectly encoded or something like this
     raw_df = pd.read_csv(data_url, sep="\s+", skiprows=22, header=None)
     data = np.hstack([raw_df.values[::2, :], raw_df.values[1::2, :2]])
     target = raw_df.values[1::2, 2]
-    df = pd.concat([pd.DataFrame(data), pd.DataFrame(target)], axis=1)
-    cor_mat = df.corr()
+    return pd.concat([pd.DataFrame(data), pd.DataFrame(target)], axis=1)
+
+def main():
+    dataset = get_dataset()
+    cor_mat = dataset.corr()
     sns.heatmap(cor_mat)
     plt.show()
 
