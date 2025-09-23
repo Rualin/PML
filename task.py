e120af335eda4a70ba686245790e289943f75931
Rualin <fateevrual@gmail.com>
2025-09-23 11:32:40 +0700

    Add columns, numeric select and picture saving

--- task.py
+++ task.py
@@ -4,10 +4,27 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def get_dataset() -> pd.DataFrame | None:
+URL = "https://lib.stat.cmu.edu/datasets/boston"
+# Dataset is downloaded without columns, so there is columns for this dataset from the internet.
+COLUMNS = [
+    "CRIM", "ZN", "INDUS", "CHAS",
+    "NOX", "RM", "AGE", "DIS",
+    "RAD", "TAX", "PTRATIO", "B",
+    "LSTAT", "MEDV",
+]
+
+def get_dataset(data_url: str, columns: list | None) -> pd.DataFrame | None:
     '''
     Function to download Boston dataset using direct link.
     
+    Arguments
+    ---------
+    data_url: str
+        The link to download the dataset from
+    columns: list | None
+        A list of column names to replace the original dataset columns with
+        or None if you don't want to replace columns
+
     Returns
     -------
     pd.Dataframe | None
@@ -16,22 +33,26 @@ def get_dataset() -> pd.DataFrame | None:
     try:
         # Function sklearn.datasets.load_boston is deprecated since scikit-learn 1.2
         # That is alternative from scikit-learn.org
-        data_url = "http://lib.stat.cmu.edu/datasets/boston"
         # 22 is number from scikit-learn.org, first lines of dataset is incorrectly encoded or something like this
         raw_df = pd.read_csv(data_url, sep="\s+", skiprows=22, header=None)
         data = np.hstack([raw_df.values[::2, :], raw_df.values[1::2, :2]])
         target = raw_df.values[1::2, 2]
-        return pd.concat([pd.DataFrame(data), pd.DataFrame(target)], axis=1)
+        res = pd.concat([pd.DataFrame(data), pd.DataFrame(target)], axis=1)
+        if columns is not None:
+            res.columns = columns
+        return res
     except Exception as e:
         print(f"Exception raised during dataset downloading and processing: {e}")
         return None
 
 def main():
     '''Main function.'''
-    dataset = get_dataset()
+    dataset = get_dataset(URL, COLUMNS)
     if dataset is not None:
+        dataset = dataset.select_dtypes(include="number")
         cor_mat = dataset.corr()
         sns.heatmap(cor_mat)
+        plt.savefig("correlation_matrix.png")
         plt.show()
 
 
