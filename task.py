259a1c3fefc1ffb8040b275f0fbaa837fe0b8602
Rualin <fateevrual@gmail.com>
2025-09-24 02:19:19 +0700

    Move cpde from main to 2 functions, update readme

--- task.py
+++ task.py
@@ -13,7 +13,7 @@ COLUMNS = [
     "LSTAT", "MEDV",
 ]
 
-def get_dataset(data_url: str, columns: list | None) -> pd.DataFrame | None:
+def get_dataset(data_url: str, columns: list | None = None) -> pd.DataFrame | None:
     '''
     Function to download Boston dataset using direct link.
     
@@ -21,7 +21,7 @@ def get_dataset(data_url: str, columns: list | None) -> pd.DataFrame | None:
     ---------
     data_url: str
         The link to download the dataset from
-    columns: list | None
+    columns: list | None, default = None
         A list of column names to replace the original dataset columns with
         or None if you don't want to replace columns
 
@@ -45,15 +45,52 @@ def get_dataset(data_url: str, columns: list | None) -> pd.DataFrame | None:
         print(f"Exception raised during dataset downloading and processing: {e}")
         return None
 
+def calculate_correlation(dataset: pd.DataFrame) -> pd.DataFrame | None:
+    '''
+    Function to find correlation matrix for numeric columns of given dataset.
+
+    Arguments
+    ---------
+    dataset: pandas.Dataframe
+        The dataset to calculate the correlation for
+    
+    Returns
+    -------
+    pd.Dataframe | None
+        Calculated correlation matrix or None if there is some exception
+    '''
+    try:
+        dataset = dataset.select_dtypes(include="number")
+        return dataset.corr()
+    except Exception as e:
+        print(f"Exception raised during correlation calculation: {e}")
+        return None
+
+def draw_heatmap(data: pd.DataFrame, save_name: str | None = None) -> None:
+    '''
+    Function to draw heatmap for given data and, optionally, to save graph.
+
+    Arguments
+    ---------
+    data: pandas.Dataframe
+        The data to draw heatmap for
+    save_name: str | None, default = None
+        Name of file to which save graph. If save_name == None or "", there will be no saving
+    '''
+    try:
+        sns.heatmap(data)
+        if save_name is not None and save_name != "":
+            plt.savefig(save_name)
+        plt.show()
+    except Exception as e:
+        print(f"Exception raised during heatmap drawing: {e}")
+
 def main():
     '''Main function.'''
     dataset = get_dataset(URL, COLUMNS)
     if dataset is not None:
-        dataset = dataset.select_dtypes(include="number")
-        cor_mat = dataset.corr()
-        sns.heatmap(cor_mat)
-        plt.savefig("correlation_matrix.png")
-        plt.show()
+        cor_mat = calculate_correlation(dataset)
+        draw_heatmap(cor_mat, "corr_mat.png")
 
 
 if __name__ == "__main__":
